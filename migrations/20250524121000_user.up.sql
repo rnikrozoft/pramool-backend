a2f3b89e5cdeeca1a6b8e3f3233be831b1965b4c
rnikrozoft <rnikrozoft.dev@gmail.com>
2025-05-27 20:52:43 +0700

    change schema

--- migrations/20250524121000_user.up.sql
+++ migrations/20250524121000_user.up.sql
@@ -4,10 +4,18 @@ SET statement_timeout = 0;
 
 CREATE TABLE users (
     user_id VARCHAR(13) PRIMARY KEY,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password TEXT NOT NULL,
+    tel VARCHAR(10) NOT NULL,
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
+    address_primary TEXT NOT NULL,
+    address TEXT,
+    soi TEXT,
+    road TEXT,
+    sub_district TEXT NOT NULL,
+    district TEXT NOT NULL,
+    province TEXT NOT NULL,
+    zip_code VARCHAR(5) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
