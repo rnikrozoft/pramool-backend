732e76da58da804b194362fe763f9989c3294c86
rnikrozoft <rnikrozoft.dev@gmail.com>
2025-06-14 16:30:07 +0700

    add IsTelAlreadyUsed handler

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -1,35 +1,44 @@
 package handler
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/rnikrozoft/pramool.in.th-backend/constant"
-	"github.com/rnikrozoft/pramool.in.th-backend/exception"
 	"github.com/rnikrozoft/pramool.in.th-backend/mapping"
-	"github.com/rnikrozoft/pramool.in.th-backend/model"
 	"github.com/rnikrozoft/pramool.in.th-backend/service"
 )
 
-type UserHandler struct {
+type userHandler struct {
 	userService service.UserService
 }
 
-func NewUserHandler(userService service.UserService) UserHandler {
-	return UserHandler{
+func NewUserHandler(userService service.UserService) userHandler {
+	return userHandler{
 		userService: userService,
 	}
 }
 
-func (h UserHandler) GetMyInformation(c *fiber.Ctx) error {
-	claims, ok := c.Locals("userClaims").(*model.CustomClaims)
-	if !ok {
-		e := exception.Set(constant.ErrorSomethingWentWrong)
-		return c.Status(fiber.StatusUnauthorized).JSON(e)
+func (h userHandler) IsTelAlreadyUsed(c *fiber.Ctx) error {
+	ok, err := h.userService.IsTelAlreadyUsed(c.Context(), c.Params("tel"))
+	if err != nil {
+		return responseCommonError(c, err)
 	}
 
-	userEntity, err := h.userService.GetMyInfo(c.Context(), claims.UserID)
+	return c.Status(fiber.StatusOK).JSON(map[string]bool{
+		"ok": ok,
+	})
+}
+
+func (h userHandler) GetMyInformation(c *fiber.Ctx) error {
+	claims, ok := c.Locals(constant.ClaimsJWTObject).(string)
+	if !ok {
+		return responseCommonError(c, errors.New("cannot claims user information"))
+	}
+
+	userEntity, err := h.userService.GetMyInfo(c.Context(), claims)
 	if err != nil {
-		e := exception.Set(constant.ErrorSomethingWentWrong)
-		return c.Status(fiber.StatusInternalServerError).JSON(e)
+		return responseCommonError(c, err)
 	}
 
 	return c.Status(fiber.StatusOK).JSON(mapping.ToUserDTO(*userEntity))
