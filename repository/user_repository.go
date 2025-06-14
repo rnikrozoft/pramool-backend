dd7da436678a77e5581e0be724241c061a649509
rnikrozoft <rnikrozoft.dev@gmail.com>
2025-06-14 16:31:21 +0700

    add FindByTel logic

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -8,6 +8,7 @@ import (
 )
 
 type UserRepository interface {
+	FindByTel(ctx context.Context, tel string) (*entity.User, error)
 	GetMyInformation(ctx context.Context, userID string) (*entity.User, error)
 }
 
@@ -20,6 +21,14 @@ func NewUserRepository(bun *bun.DB) UserRepository {
 		bun: bun,
 	}
 }
+
+func (r user) FindByTel(ctx context.Context, tel string) (*entity.User, error) {
+	user := new(entity.User)
+	query := `SELECT tel FROM users WHERE tel = ?`
+	err := r.bun.NewRaw(query, tel).Scan(ctx, user)
+	return user, err
+}
+
 func (r user) GetMyInformation(ctx context.Context, userID string) (*entity.User, error) {
 	user := new(entity.User)
 	query := `SELECT * FROM users WHERE user_id = ?`
